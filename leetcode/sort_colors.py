368cb6ebcede493b46260de983fd3db74e7d61c2
eyli <evanyeejinli@gmail.com>
2025-05-31 14:43:04 -0400

    https://leetcode.com/problems/sort-colors/

---
+++ leetcode/sort_colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeros_index = 0
+        ones_index = 0
+        twos_index = len(nums) - 1
+
+        while ones_index <= twos_index:
+            num = nums[ones_index]
+            if num == 0:
+                nums[zeros_index], nums[ones_index] = nums[ones_index], nums[zeros_index]
+                zeros_index += 1
+                ones_index += 1
+            elif num == 1:
+                ones_index += 1
+            elif num == 2:
+                nums[ones_index], nums[twos_index] = nums[twos_index], nums[ones_index]
+                twos_index -= 1
